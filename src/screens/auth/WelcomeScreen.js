1f7f7c7f5fac183fe39f5bcd84910e92583b2f21
theafolayan <theafolayan@gmail.com>
2021-01-13 15:56:16 +0100

    setup folder structure + created welcome screen

---
+++ src/screens/auth/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function WelcomeScreen() {
+    return (
+        <View>
+            <Text> Welcome Screen</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    
+})

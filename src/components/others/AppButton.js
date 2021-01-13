3b5eae16e2e30afa1482bd8bc3f730bb37c81cd2
theafolayan <theafolayan@gmail.com>
2021-01-13 20:43:03 +0100

    changed  APpButton  text color to white

--- src/components/others/AppButton.js
+++ src/components/others/AppButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import colors from '../../utilities/colors'
 
-export default function AppButton({name, onPress}) {
+export default function AppButton({name, onPress, white}) {
     return (
         <TouchableOpacity style={styles.buttonContainer} onPress = {onPress}> 
             <Text style ={styles.text}> {name}</Text>
@@ -10,20 +10,19 @@ export default function AppButton({name, onPress}) {
     )
 }
 const styles = StyleSheet.create({
-    text: {
-        textAlign: 'center',
-        // color: 'white',
-        fontSize: 14,
-        fontWeight: "700"
-    },
-    buttonContainer: {
-        // paddingVertical: 20,
-        backgroundColor: colors.primary,
-        width: 260,
-        alignContent: 'center',
-        justifyContent: 'center',
-        height: 50,
-        borderRadius: 20,
-        marginVertical: 10
-    },
-})
+  text: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#F7F7F3",
+  },
+  buttonContainer: {
+    backgroundColor: colors.primary,
+    width: 260,
+    alignContent: "center",
+    justifyContent: "center",
+    height: 50,
+    borderRadius: 20,
+    marginVertical: 10,
+  },
+});
